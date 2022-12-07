6ff20452fd096307fa522c3884f8b0187fd78c23
Martin <martin.jfreytes@gmail.com>
2022-12-07 03:39:12 +0000

    fix: GH pages relative path of fonts

--- src/fonts.tsx
+++ src/fonts.tsx
@@ -4,11 +4,11 @@ const Fonts = () => (
   <Global
     styles={`
       @font-face {
-        src: url("/fonts/Roslindale-DeckNarrowRegular.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-DeckNarrowRegular.eot?#")
+        src: url("/Roslindale-DeckNarrowRegular.eot"); /* IE < 9 */
+        src: url("/Roslindale-DeckNarrowRegular.eot?#")
             format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-DeckNarrowRegular.woff2") format("woff2"),
-          url("/fonts/Roslindale-DeckNarrowRegular.woff") format("woff");
+          /* IE 9 */ url("/Roslindale-DeckNarrowRegular.woff2") format("woff2"),
+          url("/Roslindale-DeckNarrowRegular.woff") format("woff");
         font-family: "Roslindale Deck";
         font-style: normal;
         font-weight: normal;
@@ -16,11 +16,11 @@ const Fonts = () => (
       }
       
       @font-face {
-        src: url("/fonts/Roslindale-DeckNarrowMedium.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-DeckNarrowMedium.eot?#")
+        src: url("/Roslindale-DeckNarrowMedium.eot"); /* IE < 9 */
+        src: url("/Roslindale-DeckNarrowMedium.eot?#")
             format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-DeckNarrowMedium.woff2") format("woff2"),
-          url("/fonts/Roslindale-DeckNarrowMedium.woff") format("woff");
+          /* IE 9 */ url("/Roslindale-DeckNarrowMedium.woff2") format("woff2"),
+          url("/Roslindale-DeckNarrowMedium.woff") format("woff");
         font-family: "Roslindale Deck";
         font-style: normal;
         font-weight: 500;
@@ -28,10 +28,10 @@ const Fonts = () => (
       }
       
       @font-face {
-        src: url("/fonts/Roslindale-DeckNarrowBold.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-DeckNarrowBold.eot?#") format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-DeckNarrowBold.woff2") format("woff2"),
-          url("/fonts/Roslindale-DeckNarrowBold.woff") format("woff");
+        src: url("/Roslindale-DeckNarrowBold.eot"); /* IE < 9 */
+        src: url("/Roslindale-DeckNarrowBold.eot?#") format("embedded-opentype"),
+          /* IE 9 */ url("/Roslindale-DeckNarrowBold.woff2") format("woff2"),
+          url("/Roslindale-DeckNarrowBold.woff") format("woff");
         font-family: "Roslindale Deck";
         font-style: normal;
         font-weight: bold;
@@ -39,10 +39,10 @@ const Fonts = () => (
       }
       
       @font-face {
-        src: url("/fonts/Roslindale-TextRegular.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-TextRegular.eot?#") format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-TextRegular.woff2") format("woff2"),
-          url("/fonts/Roslindale-TextRegular.woff") format("woff");
+        src: url("/Roslindale-TextRegular.eot"); /* IE < 9 */
+        src: url("/Roslindale-TextRegular.eot?#") format("embedded-opentype"),
+          /* IE 9 */ url("/Roslindale-TextRegular.woff2") format("woff2"),
+          url("/Roslindale-TextRegular.woff") format("woff");
         font-family: "Roslindale";
         font-style: normal;
         font-weight: normal;
@@ -50,10 +50,10 @@ const Fonts = () => (
       }
       
       @font-face {
-        src: url("/fonts/Roslindale-TextMedium.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-TextMedium.eot?#") format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-TextMedium.woff2") format("woff2"),
-          url("/fonts/Roslindale-TextMedium.woff") format("woff");
+        src: url("/Roslindale-TextMedium.eot"); /* IE < 9 */
+        src: url("/Roslindale-TextMedium.eot?#") format("embedded-opentype"),
+          /* IE 9 */ url("/Roslindale-TextMedium.woff2") format("woff2"),
+          url("/Roslindale-TextMedium.woff") format("woff");
         font-family: "Roslindale";
         font-style: normal;
         font-weight: 500;
@@ -61,10 +61,10 @@ const Fonts = () => (
       }
       
       @font-face {
-        src: url("/fonts/Roslindale-TextSemiBold.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-TextSemiBold.eot?#") format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-TextSemiBold.woff2") format("woff2"),
-          url("/fonts/Roslindale-TextSemiBold.woff") format("woff");
+        src: url("/Roslindale-TextSemiBold.eot"); /* IE < 9 */
+        src: url("/Roslindale-TextSemiBold.eot?#") format("embedded-opentype"),
+          /* IE 9 */ url("/Roslindale-TextSemiBold.woff2") format("woff2"),
+          url("/Roslindale-TextSemiBold.woff") format("woff");
         font-family: "Roslindale";
         font-style: normal;
         font-weight: 600;
@@ -72,10 +72,10 @@ const Fonts = () => (
       }
       
       @font-face {
-        src: url("/fonts/Roslindale-TextBold.eot"); /* IE < 9 */
-        src: url("/fonts/Roslindale-TextBold.eot?#") format("embedded-opentype"),
-          /* IE 9 */ url("/fonts/Roslindale-TextBold.woff2") format("woff2"),
-          url("/fonts/Roslindale-TextBold.woff") format("woff");
+        src: url("/Roslindale-TextBold.eot"); /* IE < 9 */
+        src: url("/Roslindale-TextBold.eot?#") format("embedded-opentype"),
+          /* IE 9 */ url("/Roslindale-TextBold.woff2") format("woff2"),
+          url("/Roslindale-TextBold.woff") format("woff");
         font-family: "Roslindale";
         font-style: normal;
         font-weight: bold;
